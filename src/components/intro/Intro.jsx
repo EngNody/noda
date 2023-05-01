84cd0782f7bb1bba7974ba24c6f97fda6414856d
EngNody <totatota1990253@gmail.com>
2023-05-01 09:51:24 +0300

    .

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,16 @@ import "./intro.scss"
 const Intro = () => {
   return (
     <div className="intro" id="intro">
+  <div className="left">
+  <div className="imgcontainer">
+    <img src="assets/frontend4.jpg" alt="" />
+  </div>
+  </div>
+  <div className="right">
+  <h2>Hi There, I'm</h2>
+  <h1>Hend Elazazzy</h1>
+  
+  </div>
     </div>
   );
 }
