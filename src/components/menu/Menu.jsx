f66ebf952afc94002e764f14376ede086933e0cd
EngNody <totatota1990253@gmail.com>
2023-05-04 08:32:03 +0300

    .

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -14,10 +14,10 @@ export default function Menu({menuOpen,setMenuOpen}) {
       <a href="#works">Works</a>
       </li>
       <li onClick={()=>setMenuOpen(false)}>
-      <a href="#contact">Contact</a>
+      <a href="#testmonials">Testmonials</a>
       </li>
       <li onClick={()=>setMenuOpen(false)}>
-      <a href="#testmonials">Testmonials</a>
+      <a href="#contact">Contact</a>
       </li>
       </ul>
     </div>
